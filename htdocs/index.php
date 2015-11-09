6cdcacd877931051b4c342907e10dd595730a52d
Zsolt Oroszlány <oroszlany.zsolt@gmail.com>
2015-11-09 19:28:29 +0100

    Ok

---
+++ htdocs/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * editorconfig-netbeans-test
+ *
+ * @package editorconfig-netbeans-test
+ * @copyright (c) 2015, Zsolt Oroszlány, All rights reserved
+ * @tutorial Hungarian specific characters éáűőúóüöíÉÁŰŐÚÓÜÖÍ
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ */
+
+
+
